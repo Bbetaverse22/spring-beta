1ba501265bf6daa07e223e18fa6f07d0015f0d95
BetulB <betul.bogrek@gmail.com>
2022-04-23 13:01:30 -0500

    many-tomany mapping, and cinemaLab

---
+++ Spring-12-OneToMany-ManyToMany/src/main/java/com/cydeo/module/PaymentDetail.java
@@ -0,0 +1,32 @@
+package com.cydeo.module;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="paymentDetails")
+@NoArgsConstructor
+@Data
+public class PaymentDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal commissionAmount;
+    private BigDecimal merchantPayoutAmount;
+    @Column(columnDefinition = "DATE")
+    private LocalDate payoutDate;
+
+    @OneToOne(mappedBy = "paymentDetail", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public PaymentDetail(BigDecimal commissionAmount, BigDecimal merchantPayoutAmount, LocalDate payoutDate) {
+        this.commissionAmount = commissionAmount;
+        this.merchantPayoutAmount = merchantPayoutAmount;
+        this.payoutDate = payoutDate;
+    }
+}
