8985d23952836e738bc2637b8c0f4b3d1d367d0c
BetulB <betul.bogrek@gmail.com>
2022-06-04 12:06:13 -0500

    new updates on api

---
+++ Spring-18-REST-ConsumingAPIs/src/main/java/com/cydeo/Spring18RestConsumingApisApplication.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Spring18RestConsumingApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring18RestConsumingApisApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
